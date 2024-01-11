2ec97871c6cefc30993f50fcd377e124009a8807
Diurno <Diurno@DESKTOP-A2FPPFB>
2024-01-11 12:57:44 +0100

    Cambios confirmados

---
+++ Proyecto1/src/paquete1/Principal.java
@@ -0,0 +1,8 @@
+package paquete1;
+
+public class Principal {
+	public static void main(String[] args){
+		//TODO Auto-Generated method sub
+		System.out.println("Ejemplo de control de versiones Git");
+	}
+}
